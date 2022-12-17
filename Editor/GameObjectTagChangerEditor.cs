e0ae3e0f27819d64b9d3196d0e0c9790d346c275
pogapoganyaa <100682474+pogapoganyaa@users.noreply.github.com>
2022-12-17 14:48:48 +0900

    List内のGameObjectのTagを変更します

---
+++ Editor/GameObjectTagChangerEditor.cs
@@ -0,0 +1,75 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace PogapogaEditor.Component
+{
+    [CustomEditor(typeof(GameObjectTagChanger))]
+    public class GameObjectTagChangerEditor : Editor
+    {
+        ReorderableList _targetObjects;
+        SerializedProperty _tag;
+        bool _isOpen = false;
+        float _spaceHeight = 10;
+
+        public override void OnInspectorGUI()
+        {
+            GameObjectTagChanger changer = (GameObjectTagChanger)target;
+
+            serializedObject.Update();
+
+            #region // Tagの表示
+            _tag = serializedObject.FindProperty("tagName");
+            EditorGUILayout.LabelField("List内のGameObjectのTagを変更します");
+            changer.tagName = EditorGUILayout.TagField("設定するTagを選択してください", _tag.stringValue);
+            #endregion
+
+            #region // Listの表示
+            SerializedProperty targetProperty = serializedObject.FindProperty(nameof(changer.targetObjects));
+            EditorGUILayout.PropertyField(targetProperty);
+            #endregion
+
+            #region // Listの並べ替え
+            _isOpen = EditorGUILayout.BeginFoldoutHeaderGroup(_isOpen, "Listの並べ替え");
+            if (_isOpen)
+            {
+                if (_targetObjects == null)
+                {
+                    _targetObjects = new ReorderableList(serializedObject, targetProperty);
+                }
+
+                _targetObjects.drawHeaderCallback = rect => EditorGUI.LabelField(rect, "編集対象のGameObject");
+                _targetObjects.drawElementCallback = (rect, index, isActive, isFcused) =>
+                {
+                    SerializedProperty elementProperty = targetProperty.GetArrayElementAtIndex(index);
+                    rect.height = EditorGUIUtility.singleLineHeight;
+                    EditorGUI.PropertyField(rect, elementProperty, new GUIContent(""));
+                };
+                _targetObjects.DoLayoutList();
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            #endregion
+
+            serializedObject.ApplyModifiedProperties();
+
+            #region // 実行用ボタン
+            EditorGUILayout.Space(_spaceHeight);
+            if (GUILayout.Button($"Tagを{changer.tagName}に設定します"))
+            {
+                if (changer.targetObjects.Count == 0) { Debug.LogWarning("GameObjectが設定されていません"); return; }
+                bool dialogFlag = EditorUtility.DisplayDialog("タグの変更", $"Tagを{changer.tagName}に設定します", "処理開始", "キャンセル");
+                if (dialogFlag == true)
+                {
+                    foreach (GameObject targetObject in changer.targetObjects) { if(targetObject != null) { Undo.RecordObject(targetObject, "Tagの変更"); } }
+                    changer.ChangeGameObjectTag();
+                }
+            }
+            EditorGUILayout.Space(_spaceHeight);
+            if (GUILayout.Button("List内のGameObjectを選択状態にします")) { Selection.objects = changer.targetObjects.ToArray(); }
+            EditorGUILayout.Space(_spaceHeight);
+            #endregion
+        }
+    }
+}
+#endif
---
+++ GameObjectTagChanger.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PogapogaEditor.Component
+{
+    public class GameObjectTagChanger : MonoBehaviour
+    {
+        [HideInInspector] public string tagName = "Untagged";
+        public List<GameObject> targetObjects = new List<GameObject>();
+
+        public void ChangeGameObjectTag()
+        {
+            foreach (GameObject targetObject in targetObjects)
+            {
+                if (targetObject == null)
+                    continue;
+                targetObject.tag = tagName;
+            }
+        }
+    }
+}
+#endif
